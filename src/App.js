c43b8865d989973b718b748cdeba22e32f51cb26
zahraa <me@example.com>
2025-10-28 20:43:58 +0300

    new commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import Header from "./components/Navs/Header";
+import React, { Suspense, lazy } from "react";
+//import Header from "./components/Navs/Header";
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/pages/Home";
 import Allbbooks from "./components/pages/Allbbooks";
 import Memos from "./components/pages/Memos";
 import Newbooks from "./components/pages/Newbooks";
 import Reviews from "./components/pages/Reviews";
+import Placeholder from "react-bootstrap/Placeholder";
 function App() {
+  const LazyHeader = lazy(() => import("./components/Navs/Header"));
   return (
     <>
-      <Header />
+      <Suspense
+        fallback={
+          <Placeholder as="div" animation="glow">
+            <Placeholder xs={12} style={{ height: "150px" }} />
+          </Placeholder>
+        }
+      >
+        <LazyHeader />
+      </Suspense>
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/allbooks" element={<Allbbooks />} />
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
-
+import { Placeholder } from "react-bootstrap";
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      Home
+     
+    </div>
+  );
 }
 
 export default Home;
